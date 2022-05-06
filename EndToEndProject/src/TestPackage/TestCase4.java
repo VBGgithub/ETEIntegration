eefbdfc8a54c39c16a2985bb40969bce1385242c
vrinda <vrindajagtap@gmail.com>
2022-05-06 13:02:10 +0530

    Add Test case 4

---
+++ EndToEndProject/src/TestPackage/TestCase4.java
@@ -0,0 +1,15 @@
+package TestPackage;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestCase4 {
+	@Test
+	public void function() {
+		System.setProperty("webdriver.chrome.driver",
+				"I:\\IMP Backup\\05_Vrinda Office data\\Cogniwise\\Training\\Drivers\\chromedriver.exe");
+		ChromeDriver browser = new ChromeDriver();
+		browser.get("https://www.google.com");
+		browser.quit();
+	}
+}
